16a57cc9044a3162a2ea6c11783c1f022d53623c
Anas <anas7.cule@gmail.com>
2022-05-04 22:07:59 +0800

    Implemented POST request handler

--- frontend/views.py
+++ frontend/views.py
@@ -1,9 +1,62 @@
+from shutil import ExecError
 import sqlite3
 from django.shortcuts import render
 
 
 def index(request):
     index_dict = {}
+    msg = ''
+    try:
+        if request.method == 'POST':
+            
+            where_clause = ''
+            
+            if request.POST.get('radio_poet'):
+            
+                # get search string
+                poet_name = request.POST.get('poet_name') or None
+                if poet_name is None:
+                    raise Exception('Poet name is mandatory')
+                                
+                # construct where clause
+                where_clause += f"poet LIKE '%{poet_name}%'"
+            
+            elif request.POST.get('radio_bayt'):
+                
+                # get search string
+                keyword = request.POST.get('keyword') or None
+                if keyword is None:
+                    raise Exception('Search keyword is mandatory')
+                
+                # get additional parameters
+                bahr = request.POST.get('bahr_dd') or None
+                age = request.POST.get('age_dd') or None
+                
+                # construct where clause
+                where_clause += f"bayt LIKE '%{keyword}%'"
+                
+                if bahr is not None:
+                    where_clause += f" AND bahr = '{bahr}'"
+
+                if age is not None:
+                    where_clause += f" AND age = '{age}'"
+                    
+            else:
+                raise Exception('Unknown search mode. Contact IT')
+            
+            conn = sqlite3.connect('db.sqlite3')
+            c = conn.cursor()
+            c.execute(f"SELECT * FROM poem_dataset WHERE {where_clause}")
+            queryset = c.fetchall()
+            c.close()
+                
+            index_dict['search_results'] = queryset
+            
+    except Exception as e:
+        msg = str(e)
+    
+    index_dict['msg'] = msg
+    
     return render(request, 'index.html', context=index_dict)
 
 
