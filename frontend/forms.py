afd7cbaef748bf3330a6eea66bf89c1e123883ca
Ebrahim Sadri <ebrahimsadary@gmail.com>
2022-05-21 23:24:09 +0300

    log in fully functional, temp profile page added, signup needs tweaking

--- frontend/forms.py
+++ frontend/forms.py
@@ -1,4 +1,31 @@
 from django import forms
 from django.core import validators
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your forms here.
+class NewUserForm(UserCreationForm):
+
+    email = forms.EmailField(required=True)
+
+    class Meta():
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+    def clean_username(self, *args, **kwargs):
+        username = self.cleaned_data['username']
+        if len(username) > 150:
+            raise
+        valid_symbols = ["@",".","+","-","_" ]
+        check_symbols = [x for x in username if x in valid_symbols]
+        if check_symbols:
+            raise
+        
--- frontend/views.py
+++ frontend/views.py
@@ -2,31 +2,40 @@ from shutil import ExecError
 import sqlite3
 from django.shortcuts import render
 
+from django.contrib import messages
+from frontend.forms import NewUserForm
+
+# login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+
 
 def index(request):
     index_dict = {}
     msg = ''
     try:
         if request.method == 'POST':
-            
+
             where_clause = ''
-            
+
             if request.POST.get('radio_poet'):
-            
+
                 # get search string
                 poet_name = request.POST.get('poet_name') or None
                 if poet_name is None:
                     raise Exception('Poet name is mandatory')
-                                
+
                 # construct where clause
                 where_clause += f"poet LIKE '%{poet_name}%'"
-            
+
                 conn = sqlite3.connect('db.sqlite3')
                 c = conn.cursor()
                 c.execute(f"SELECT DISTINCT poet FROM poem_dataset WHERE {where_clause}")
                 queryset = c.fetchall()
                 c.close()
-                
+
                 i = 0
                 poet_list = []
                 row = []
@@ -40,49 +49,49 @@ def index(request):
                     i += 1
                     if i == 8:
                         break
-                    
+
                 index_dict['poet_list'] = poet_list
-                
+
                 index_dict['search_params'] = {
                     'poet_name': poet_name
                 }
-                
+
             elif request.POST.get('radio_bayt'):
-                
+
                 # get search string
                 keyword = request.POST.get('keyword') or None
                 if keyword is None:
                     raise Exception('Search keyword is mandatory')
-                
+
                 # get additional parameters
                 bahr = request.POST.get('bahr_dd') or None
                 age = request.POST.get('age_dd') or None
-                
+
                 # construct where clause
                 where_clause += f"bayt LIKE '%{keyword}%'"
-                
+
                 if bahr is not None:
                     where_clause += f" AND bahr = '{bahr}'"
 
                 if age is not None:
                     where_clause += f" AND age = '{age}'"
-                
+
                 conn = sqlite3.connect('db.sqlite3')
                 c = conn.cursor()
                 c.execute(f"SELECT * FROM poem_dataset WHERE {where_clause}")
                 queryset = c.fetchall()
                 c.close()
-                    
+
                 index_dict['search_results'] = queryset
-                
+
             else:
                 raise Exception('Unknown search mode. Contact IT')
-            
+
     except Exception as e:
         msg = str(e)
-    
+
     index_dict['msg'] = msg
-    
+
     return render(request, 'index.html', context=index_dict)
 
 
@@ -101,20 +110,60 @@ def library(request):
     return render(request, 'library.html', context=library_dict)
 
 
-def search(request):
-    search_dict = {}
-
+def user_signup(request):
     if request.method == 'POST':
-        search_val = request.POST['search_val']
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Signup Successful')
+            return render(request, 'index.html', context={})
 
-        conn = sqlite3.connect('db.sqlite3')
-        c = conn.cursor()
-        c.execute(f"SELECT * FROM poem_dataset WHERE bayt LIKE '%{search_val}%' ")
-        q_out = c.fetchall()
-        c.close()
+        messages.error(request, "Unsuccessful Signup. Invalid information.")
 
-        search_dict = {'results': q_out}
-        return render(request, 'search_results.html', context=search_dict)
+    signup_form = NewUserForm()
+    return render(request, 'signup.html', context={"signup_form":signup_form})
 
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('profile'))
     else:
-        return render(request, 'search_results.html', context=search_dict)
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(reverse('profile'))
+            return HttpResponse("Username or password entered is incorrect")
+        else:
+            return render(request, 'login.html', context={})
+
+
+@login_required
+def profile_page(request):
+    return render(request, 'profile.html', context={})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+
+# def search(request):
+#     search_dict = {}
+#     if request.method == 'POST':
+#         search_val = request.POST['search_val']
+#         conn = sqlite3.connect('db.sqlite3')
+#         c = conn.cursor()
+#         c.execute(f"SELECT * FROM poem_dataset WHERE bayt LIKE '%{search_val}%' ")
+#         q_out = c.fetchall()
+#         c.close()
+#         search_dict = {'results': q_out}
+#         return render(request, 'search_results.html', context=search_dict)
+#     else:
+#         return render(request, 'search_results.html', context=search_dict)
