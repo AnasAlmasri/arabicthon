da15b37732cdf25cadbbba3f592f9849879dd94d
Khaled alazem <64016097+khaledrazem@users.noreply.github.com>
2022-05-22 18:27:13 +0300

    Add files via upload

---
+++ SEMETIC ANAL SIS/ModelWrapper.py
@@ -0,0 +1,32 @@
+from DICKtionary import WordInterpretation
+from databuttwiperpanda import ButtWiper
+from preprocess import datapreprocessing
+from LOGICREGRESSION import LOGREG
+from scraper0 import poemscraper
+import pickle 
+
+class ModelWrapper:
+
+    model_name = "jhdjskvldskvnsd/njsdvnsj"
+
+    @staticmethod
+    def predict_logreg(to_predict,model):
+       return model.predict([datapreprocessing.preprocess(to_predict)])
+       
+
+    @staticmethod
+    def TrainModel():
+        LOGREG.TrainModel()
+        
+    @staticmethod
+    def CreateData():
+        poemscraper.ScrapeIt()
+        ButtWiper.WipeTheButt()
+    
+    @staticmethod
+    def GetMeaning(word):
+        return(WordInterpretation.getMeaning(word))
+    
+
+
+    
