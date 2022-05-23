2e823da9cbb53f711c1b836419295d08a79c58c1
Anas <anas7.cule@gmail.com>
2022-05-23 22:16:16 +0800

    Refactored preprocess.py

--- sentiment_analysis/preprocess.py
+++ sentiment_analysis/preprocess.py
@@ -3,18 +3,21 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem.isri import ISRIStemmer
 from nltk.tokenize import word_tokenize
+
 # libraries for data split and feature extraction
 
-class datapreprocessing:
-        
+
+class DataPreprocessing:
+
     # first we define a list of arabic and english punctiations that we want to get rid of in our text
-    
-    punctuations = '''`÷×؛<>_()*&^%][ـ،/:"؟.,'{}~¦+|!”…“–ـ''' + string.punctuation
-    
+
+    punctuations = """`÷×؛<>_()*&^%][ـ،/:"؟.,'{}~¦+|!”…“–ـ""" + string.punctuation
+
     # Arabic stop words with nltk
     stop_words = stopwords.words("arabic")
-    
-    arabic_diacritics = re.compile("""
+
+    arabic_diacritics = re.compile(
+        """
                                  ّ    | # Shadda
                                  َ    | # Fatha
                                  ً    | # Tanwin Fath
@@ -24,39 +27,42 @@ class datapreprocessing:
                                  ٍ    | # Tanwin Kasr
                                  ْ    | # Sukun
                                  ـ     # Tatwil/Kashida
-                             """, re.VERBOSE)
+                             """,
+        re.VERBOSE,
+    )
     stemmer = ISRIStemmer()
+
     def preprocess(text):
-        
-        '''
+
+        """
         text is an arabic string input
-        
+
         the preprocessed text is returned
-        '''
-        
-      
-        
-        #remove punctuations
-        translator = str.maketrans('', '', datapreprocessing.punctuations)
+        """
+
+        # remove punctuations
+        translator = str.maketrans("", "", DataPreprocessing.punctuations)
         text = text.translate(translator)
-        
+
         # remove Tashkeel
-        text = re.sub(datapreprocessing.arabic_diacritics, '', text)
-        
-        #remove longation
+        text = re.sub(DataPreprocessing.arabic_diacritics, "", text)
+
+        # remove longation
         text = re.sub("[إأآا]", "ا", text)
         text = re.sub("ى", "ي", text)
         text = re.sub("ؤ", "ء", text)
         text = re.sub("ئ", "ء", text)
         text = re.sub("ة", "ه", text)
         text = re.sub("گ", "ك", text)
-        
-        text = ' '.join(datapreprocessing.stemmer.stem(word) for word in text.split() if datapreprocessing.stemmer.stem(word) not in datapreprocessing.stop_words)
-        # text = word_tokenize(text)
-        # text = ' '.join(text) 
+
+        text = " ".join(
+            DataPreprocessing.stemmer.stem(word)
+            for word in text.split()
+            if DataPreprocessing.stemmer.stem(word) not in DataPreprocessing.stop_words
+        )
+
         text = word_tokenize(text)
-    
-        
-        text = ' '.join(text) 
-    
+
+        text = " ".join(text)
+
         return text
